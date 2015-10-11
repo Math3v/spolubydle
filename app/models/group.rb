0aff1160cbe50073640d99109ca8097dbdf1d843
Matej Minarik <matej.minarik@agile-ict.com>
2015-10-11 12:17:26 +0200

    Generate unique code on Group creation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,16 @@
 class Group < ActiveRecord::Base
-  validates :name, :code, presence: true
+  validates :name, presence: true
   validates :code, uniqueness: true
+
+  before_create :generate_code
+
+  private
+
+    def generate_code
+      while true
+        code = rand(36**8).to_s(36)
+        break unless Group.exists?(code: code)
+      end
+      self.code = code
+    end
 end
