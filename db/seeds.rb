546be3d68b42ccc5b34d5727acf37862f6d48b7d
Matej Minarik <matej.minarik@agile-ict.com>
2015-11-29 15:02:56 +0100

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ m1 = Member.find_or_create_by(name: 'TestMember1', admin: true, group_id: g.id)
 m2 = Member.find_or_create_by(name: 'TestMember2', admin: false, group_id: g.id)
 m3 = Member.find_or_create_by(name: 'TestMember3', admin: false, group_id: nil)
 
-Task.find_or_create_by(description: 'Wash dishes', due_date: Date.new(2015, 12, 24), group_id: g.id, member_id: m1.id)
-Task.find_or_create_by(description: 'Wash floors', due_date: Date.new(2015, 12, 25), group_id: g.id, member_id: m2.id)
+Task.find_or_create_by(description: 'Wash dishes', due_date: Date.new(2015, 12, 24), group_id: g.id, member_id: m1.id, points: 200, points_original: 200, done: false)
+Task.find_or_create_by(description: 'Wash floors', due_date: Date.new(2015, 12, 25), group_id: g.id, member_id: m2.id, points: 100, points_original: 100, done: true)
