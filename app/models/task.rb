b3c9389e323e09cc4777d2879a806182e5d49a87
Matej Minarik <matej.minarik@agile-ict.com>
2015-11-30 16:46:59 +0100

    Due date returned as object

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,21 @@ class Task < ActiveRecord::Base
 
   before_create :init
 
+  def due_date_date
+    ret = {}
+    date = self.due_date
+    ret[:year] = date.year
+    ret[:month] = date.month
+    ret[:day] = date.day
+    ret
+  end
+
+  def as_json(options = {})
+    obj = super(except: :due_date)
+    obj[:due_date] = due_date_date
+    obj
+  end
+
   private
 
     def init
